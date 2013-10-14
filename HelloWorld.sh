de74ce961d3f4c9b734a90f691d9bc4f8e503abd
Diego Waxemberg <diego.waxemberg@gmail.com>
2013-10-14 11:26:50 -0400

    added pause and info message

--- HelloWorld.sh
+++ HelloWorld.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+function pause() {
+  read -p "$*"
+}
+
+echo "All compiled languages will be compiled before running. Any language that
+cannot be run will simply display the code."
+pause "Press [Enter] key to continue..."
 echo "0. Bash"
 echo "Hello World"
 echo "1. Java"
@@ -13,3 +20,5 @@ echo "3. Python"
 python Python/HelloWorld.py
 echo "4. Ruby"
 ruby Ruby/HelloWorld.rb
+echo "5. BASIC"
+cat Basic/HelloWorld.bas
